8985271922c879064307d0ba35192bb27ceca9e2
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-03-26 14:10:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+ // code your solution here
+console.log(saturdayFun("bathe my dog"));
+console.log(saturdayFun());
+ 
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+ 
+ 
+let mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+console.log(mondayWork());
+ 
+
+let wrapAdjective = function(kdot="*"){
+    return function(wish="special"){
+        return(`You are ${kdot}${wish}${kdot}!`)
+    }
+}
+console.log(wrapAdjective()("a hard worker"));
+console.log(wrapAdjective()("||a dedicated programmer||"))
